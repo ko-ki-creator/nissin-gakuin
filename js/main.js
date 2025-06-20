6c5225719cf4ce4b5ad6be1210d1c111888ba171
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-06-20 10:35:44 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(function () {
   // ハンバーガーボタンクリックで実行
-        $("#js-hamburger").click(function () {
+    $("#js-hamburger").click(function () {
         $(this).toggleClass("active");
         $("#js-nav").toggleClass("active");
     });
 
     // メニュー項目のクリック時にメニューを閉じる
-        $(".nav-item a").click(function () {
+    $(".nav-item a").click(function () {
         $("#js-hamburger").removeClass("active");
         $("#js-nav").removeClass("active");
     });
